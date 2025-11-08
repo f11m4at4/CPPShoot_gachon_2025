1f2ec52eb339a68fa2f926e2db9dbaa25bb3ce56
f11m4at4 <f11m4at4@gmail.com>
2025-11-08 15:44:49 +0900

    Initial commit

---
+++ Source/CPPShoot/Private/CPPShoot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPPShoot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPShoot, "CPPShoot" );
---
+++ Source/CPPShoot/Public/CPlayer.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "CPlayer.generated.h"
+
+UCLASS()
+class CPPSHOOT_API ACPlayer : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACPlayer();
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	UPROPERTY(VisibleAnywhere, Category=MyComponent, meta=(AllowPrivateAccess=true))
+	UBoxComponent* boxComp;
+};
